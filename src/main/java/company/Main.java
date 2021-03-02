4fbc4ccc3e0c6122ce94d9235bf060e696c5f388
Kristian Smolko <kristian.smolko@kosickaakademia.sk>
2021-03-02 12:59:31 +0100

    Updated main

--- src/main/java/company/Main.java
+++ src/main/java/company/Main.java
@@ -1,7 +1,10 @@
 package company;
 
+import company.database.Database;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Database dat = new Database();
+        dat.getConnection();
     }
 }
