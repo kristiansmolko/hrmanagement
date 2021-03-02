63b5d6bf2a63ad34f90bf6be4c0d679d2c9059b9
Kristian Smolko <kristian.smolko@kosickaakademia.sk>
2021-03-02 12:59:12 +0100

    Added log to print messages

---
+++ src/main/java/company/log/Log.java
@@ -0,0 +1,13 @@
+package company.log;
+
+public class Log {
+    public void error(String msg){
+        System.out.println("[ERROR] : " + msg);
+    }
+
+    public void print(String msg){
+        System.out.println("[OK] : " + msg);
+    }
+
+
+}
