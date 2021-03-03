9e93af6d8fe539470b26823b316483b46fdcb546
Kristian Smolko <kristian.smolko@kosickaakademia.sk>
2021-03-03 11:41:13 +0100

    Added new methods: getFemales, getMales and executeSelect

--- src/main/java/company/database/Database.java
+++ src/main/java/company/database/Database.java
@@ -4,10 +4,9 @@ import company.entity.User;
 import company.log.Log;
 
 import java.io.InputStream;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 public class Database {
@@ -43,16 +42,58 @@ public class Database {
 
     public boolean insertNewUser(User user){
         try (Connection connection = getConnection()){
-            PreparedStatement ps = connection.prepareStatement(insertQuery);
-            ps.setString(1, user.getFname());
-            ps.setString(2, user.getLname());
-            ps.setInt(3, user.getAge());
-            ps.setInt(4, user.getGender().getValue() == 0 ? 0 : user.getGender().getValue() == 1 ? 1 : 2);
-            int result = ps.executeUpdate();
-            log.print("Added new user");
-            close(connection);
-            return result == 1;
+            if (connection != null) {
+                PreparedStatement ps = connection.prepareStatement(insertQuery);
+                ps.setString(1, user.getFname());
+                ps.setString(2, user.getLname());
+                ps.setInt(3, user.getAge());
+                ps.setInt(4, user.getGender().getValue() == 0 ? 0 : user.getGender().getValue() == 1 ? 1 : 2);
+                int result = ps.executeUpdate();
+                log.print("Added new user");
+                return result == 1;
+            }
         } catch (Exception e) { log.error(e.toString()); }
         return false;
+    }
+
+    public List<User> getFemales(){
+        String query = "SELECT * FROM user WHERE gender = 1";
+        try (Connection connection = getConnection()){
+            if (connection != null) {
+                PreparedStatement ps = connection.prepareStatement(query);
+                return executeSelect(ps);
+            }
+        } catch (Exception e) { log.error(e.toString()); }
+        return null;
+    }
+
+    public List<User> getMales(){
+        String query = "SELECT * FROM user WHERE gender = 0";
+        try (Connection connection = getConnection()){
+            if (connection != null) {
+                PreparedStatement ps = connection.prepareStatement(query);
+                return executeSelect(ps);
+            }
+        } catch (Exception e) { log.error(e.toString()); }
+        return null;
+    }
+
+    public List<User> getUsersByAge(int from, int to){
+        return null;
+    }
+
+    private List<User> executeSelect(PreparedStatement ps) throws SQLException {
+        ResultSet rs =  ps.executeQuery();
+        List<User> list = new ArrayList<>();
+        while (rs.next()){
+            String fname = rs.getString("fname");
+            String lname = rs.getString("lname");
+            int age = rs.getInt("age");
+            int gender = rs.getInt("gender");
+            int id = rs.getInt("id");
+            list.add(new User(id, fname, lname, age, gender));
+        }
+        log.info("Number of records: " + list.size());
+        return list;
     }
 }
