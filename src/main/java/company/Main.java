fa3912cbb4abe033f4542448f4297711bfd87cf6
Kristian Smolko <kristian.smolko@kosickaakademia.sk>
2021-03-04 13:15:24 +0100

    Modified main

--- src/main/java/company/Main.java
+++ src/main/java/company/Main.java
@@ -1,10 +1,15 @@
 package company;
 
 import company.database.Database;
+import company.entity.User;
+import company.enums.Gender;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         Database dat = new Database();
-        dat.getConnection();
+        //dat.insertNewUser(new User("John", "Wick", 30, Gender.MALE.getValue()));
+        dat.getUser("John");
     }
 }
