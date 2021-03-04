10273f13abc5fea423bd337cc24a4d28adde07dd
Kristian Smolko <kristian.smolko@kosickaakademia.sk>
2021-03-04 13:14:44 +0100

    Added methods: changeAge and getUser

--- src/main/java/company/database/Database.java
+++ src/main/java/company/database/Database.java
@@ -16,6 +16,8 @@ public class Database {
     private final String getUsersByAge = "SELECT * FROM user WHERE age BETWEEN ? AND ?";
     private final String getUserById = "SELECT * FROM user WHERE id = ?";
     private final String getAllUsers = "SELECT * from user";
+    private final String updateAge = "UPDATE user SET age = ? WHERE id = ?";
+    private final String getUserByPattern = "SELECT * FROM user WHERE lname LIKE ? OR fname LIKE ?";
 
     public Connection getConnection(){
         try {
@@ -76,18 +78,22 @@ public class Database {
         if (from < 0 || to < 0 || from > to)
             return null;
         try (Connection connection = getConnection()){
-            PreparedStatement ps = connection.prepareStatement(getUsersByAge);
-            ps.setInt(1, from);
-            ps.setInt(2, to);
-            return executeSelect(ps);
+            if (connection != null) {
+                PreparedStatement ps = connection.prepareStatement(getUsersByAge);
+                ps.setInt(1, from);
+                ps.setInt(2, to);
+                return executeSelect(ps);
+            }
         } catch (Exception e) { log.error(e.toString()); }
         return null;
     }
 
     public List<User> getAllUsers(){
         try (Connection connection = getConnection()){
-            PreparedStatement ps = connection.prepareStatement(getAllUsers);
-            return executeSelect(ps);
+            if (connection != null) {
+                PreparedStatement ps = connection.prepareStatement(getAllUsers);
+                return executeSelect(ps);
+            }
         } catch (Exception e) { log.error(e.toString()); }
         return null;
     }
@@ -98,9 +104,11 @@ public class Database {
             return null;
         }
         try (Connection connection = getConnection()){
-            PreparedStatement ps = connection.prepareStatement(getUserById);
-            ps.setInt(1, id);
-            return executeSelect(ps);
+            if (connection != null) {
+                PreparedStatement ps = connection.prepareStatement(getUserById);
+                ps.setInt(1, id);
+                return executeSelect(ps);
+            }
         } catch (Exception e) { log.error(e.toString()); }
         return null;
     }
@@ -118,5 +126,35 @@ public class Database {
         }
         log.info("Number of records: " + list.size());
         return list;
+    }
+
+    public boolean changeAge(int id, int age){
+        if (id < 0 || age < 1 || age >= 100)
+            return false;
+        try (Connection connection = getConnection()){
+            if (connection != null) {
+                PreparedStatement ps = connection.prepareStatement(updateAge);
+                ps.setInt(1, age);
+                ps.setInt(2, id);
+                int update = ps.executeUpdate();
+                log.print("Updated age for id: " + id);
+                return update == 1;
+            }
+        } catch (Exception e) { log.error(e.toString()); }
+        return false;
+    }
+
+    public List<User> getUser(String pattern){
+        try (Connection connection = getConnection()){
+            if (connection != null){
+                PreparedStatement ps = connection.prepareStatement(getUserByPattern);
+                String newPattern = "%" + pattern + "%";
+                ps.setString(1, newPattern);
+                ps.setString(2, newPattern);
+                return executeSelect(ps);
+            }
+        } catch (Exception e) { log.error(e.toString()); }
+
+        return null;
     }
 }
