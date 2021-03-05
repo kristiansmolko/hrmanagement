78195c8c768028ddf8da2b94f422c5023176ee04
Kristian Smolko <kristian.smolko@kosickaakademia.sk>
2021-03-05 11:04:41 +0100

    Added methods getJson, getCurrentDateTime and normalizeName

--- src/main/java/company/utill/Util.java
+++ src/main/java/company/utill/Util.java
@@ -1,5 +1,69 @@
 package company.utill;
 
-public class Util {
+import company.entity.User;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Util {
+    public String getJson(List<User> list){
+        if (list.isEmpty())
+            return "{}";
+        JSONObject object = new JSONObject();
+        object.put("datetime", getCurrentDateTime());
+        object.put("size", list.size());
+        JSONArray array = new JSONArray();
+        for (User user : list) {
+            JSONObject jsonUser = new JSONObject();
+            jsonUser.put("id", user.getId());
+            jsonUser.put("fname", user.getFname());
+            jsonUser.put("lname", user.getLname());
+            jsonUser.put("age", user.getAge());
+            jsonUser.put("gender", user.getGender().toString());
+            array.add(jsonUser);
+        }
+        object.put("users", array);
+        return object.toJSONString();
+    }
+
+    public String getJson(User user){
+        if (user == null)
+            return "{}";
+        JSONObject object = new JSONObject();
+        object.put("datetime", getCurrentDateTime());
+        object.put("size", 1);
+        JSONArray array = new JSONArray();
+        JSONObject jsonUser = new JSONObject();
+        jsonUser.put("id", user.getId());
+        jsonUser.put("fname", user.getFname());
+        jsonUser.put("lname", user.getLname());
+        jsonUser.put("age", user.getAge());
+        jsonUser.put("gender", user.getGender().toString());
+        array.add(jsonUser);
+        object.put("users", array);
+        return object.toJSONString();
+    }
+
+    public String getCurrentDateTime(){
+        Date today = new Date();
+        DateFormat format = new SimpleDateFormat("yyyy-MM-dd hh-mm-ss");
+        System.out.println(format.format(today));
+        return format.format(today); //2021-03-05 15:07:23
+    }
+
+    public String normalizeName(String name){
+        if (name == null || name.equals(""))
+            return "";
+        name = name.trim();
+        String partOfName = name.replace(String.valueOf(name.charAt(0)), "");
+        String newName = Character.toUpperCase(name.charAt(0)) + partOfName.toLowerCase();
+        return newName;
+        //or better
+        //return Character.toUpperCase(name.charAt(0)) + name.substring(1).toLowerCase()
+    }
 }
