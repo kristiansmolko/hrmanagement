9d74890dd6dc00b502eb215d541524b41f1a1356
Kristian Smolko <kristian.smolko@kosickaakademia.sk>
2021-03-12 11:08:45 +0100

    Added application server class

---
+++ src/main/java/company/App.java
@@ -0,0 +1,13 @@
+package company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "company.controller")
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
