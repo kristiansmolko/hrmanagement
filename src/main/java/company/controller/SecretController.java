5ced6d1f046dd7bb78bf7603c6e4dc27d27ecd7e
Kristian Smolko <kristian.smolko@kosickaakademia.sk>
2021-03-19 11:11:59 +0100

    Added secret method

--- src/main/java/company/controller/SecretController.java
+++ src/main/java/company/controller/SecretController.java
@@ -1,24 +1,36 @@
 package company.controller;
 
 import company.log.Log;
+import company.utill.Util;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class SecretController {
     Log log = new Log();
+    Util util = new Util();
+    Map<String, String> map = new HashMap();
     private final String password = "Kosice2021!";
+    private ResponseEntity.BodyBuilder unauthorized = ResponseEntity.status(401);
+    private ResponseEntity.BodyBuilder badRequest = ResponseEntity.status(400);
+    private ResponseEntity.BodyBuilder ok = ResponseEntity.status(200);
 
     @GetMapping("/secret")
-    public String secret(){
-        return "secret";
+    public ResponseEntity<String> secret(@RequestHeader("token") String token){
+        if (!util.authorizeToken(token))
+            return badRequest.body("Wrong token");
+        String tokenNum = token.substring(7);
+        for (Map.Entry<String, String> entry : map.entrySet())
+            if (entry.getValue().equals(tokenNum))
+                return ok.body("Welcome message for " + entry.getKey());
+        return unauthorized.body("Unauthorized access");
     }
 
     @PostMapping("/login")
@@ -35,7 +47,13 @@ public class SecretController {
                 log.error("Wrong password");
                 return ResponseEntity.status(401).body("Wrong password");
             }
-            return ResponseEntity.status(200).body("Welcome " + login);
+            String token = util.generateToken();
+            map.put(login, token);
+            JSONObject json = new JSONObject();
+            json.put("login", login);
+            json.put("token", "Bearer " + token);
+
+            return ResponseEntity.status(200).contentType(MediaType.APPLICATION_JSON).body(json.toJSONString());
         } catch (ParseException e) { e.printStackTrace(); }
         return null;
     }
