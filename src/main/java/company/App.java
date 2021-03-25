0f13f1e8ad73f968de900a986b85d95fcb7a0b45
Kristian Smolko <kristian.smolko@kosickaakademia.sk>
2021-03-25 10:08:20 +0100

    Modified app to get port

--- src/main/java/company/App.java
+++ src/main/java/company/App.java
@@ -4,10 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import java.util.Collections;
+
 @SpringBootApplication
 @ComponentScan(basePackages = "company.controller")
 public class App {
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        SpringApplication app = new SpringApplication(App.class);
+        app.setDefaultProperties(Collections.singletonMap("server.port", args[0]));
+        app.run(args);
+        System.out.println(args[0]);
     }
 }
