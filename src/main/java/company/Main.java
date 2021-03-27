290388b001eab4b37e313c1b1aa400edf21ed347
Kristian Smolko <kristian.smolko@kosickaakademia.sk>
2021-03-27 16:51:04 +0100

    Nothing important

--- src/main/java/company/Main.java
+++ src/main/java/company/Main.java
@@ -1,20 +1,7 @@
 package company;
 
-import company.database.Database;
-import company.entity.User;
-import company.enums.Gender;
-import company.utill.Util;
-
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
-        Database dat = new Database();
-        Util util = new Util();
-        //System.out.println(util.getJson(dat.getMales()));
-        User user = new User("jANKO      ", "     hRaSkO      ", 10, Gender.MALE.getValue());
-        System.out.println(user.toString());
-        //dat.insertNewUser(new User("John", "Wick", 30, Gender.MALE.getValue()));
-        //dat.getUser("John");
+
     }
 }
