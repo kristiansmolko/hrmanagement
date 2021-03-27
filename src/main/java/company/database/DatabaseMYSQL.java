f06367a32d04accc21c164381b9aaf78df3b7435
Kristian Smolko <kristian.smolko@kosickaakademia.sk>
2021-03-27 16:52:48 +0100

    Removed database to DatabaseMYSQL to differ from the other database class

--- src/main/java/company/database/DatabaseMYSQL.java
+++ src/main/java/company/database/DatabaseMYSQL.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
-public class Database {
+public class DatabaseMYSQL {
     Log log = new Log();
     private final String insertQuery = "INSERT INTO user (fname, lname, age, gender) " +
             "VALUES (?, ?, ?, ?)";
