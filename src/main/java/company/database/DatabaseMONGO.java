a6fbe1b80c97bbd0b71dcde88c3774246bfd9ef9
Kristian Smolko <kristian.smolko@kosickaakademia.sk>
2021-03-27 16:52:12 +0100

    Added database class to connect to mongodb and insertUser and getUsers methods

---
+++ src/main/java/company/database/DatabaseMONGO.java
@@ -0,0 +1,51 @@
+package company.database;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoIterable;
+import company.entity.User;
+import company.log.Log;
+import org.bson.Document;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DatabaseMONGO {
+    Log log = new Log();
+    private static final MongoClient mongoClient = new MongoClient();
+    private static MongoDatabase database;
+    private static MongoCollection<Document> test;
+
+    public void insertUser(JSONObject object){
+        database = mongoClient.getDatabase("myFirstDb");
+        test = database.getCollection("users");
+        object.put("gender",
+                object.get("gender") == null ? 2 : object.get("gender").equals("female") ? 1 : 0);
+        Document doc = Document.parse(object.toString());
+        test.insertOne(doc);
+        log.print("User added to mongodb");
+    }
+
+    public List<User> getUsers(){
+        List<User> list = new ArrayList<>();
+        database = mongoClient.getDatabase("myFirstDb");
+        test = database.getCollection("users");
+        MongoCollection<Document> table = test;
+        for (Document doc : table.find()){
+            try {
+                JSONObject object = (JSONObject) new JSONParser().parse(doc.toJson());
+                list.add(new User((String) object.get("fname"),
+                        (String) object.get("lname"),
+                        Integer.parseInt(String.valueOf(object.get("age"))),
+                        Integer.parseInt(String.valueOf(object.get("gender")))
+                ));
+            } catch (ParseException e) { e.printStackTrace(); }
+        }
+        return list;
+    }
+}
